29046f817080cc6b9bbf721365eea81410e7a1fd
Jeff <jeffsee.55@gmail.com>
2014-10-31 16:55:56 -0400

    Fixed inquiry mailer

--- app/mailers/inquiry_notice.rb
+++ app/mailers/inquiry_notice.rb
@@ -2,6 +2,7 @@ class InquiryNotice < ActionMailer::Base
   default from: "inquiries@tennis_advantage.com"
 
   def inquiry_email(inquiry)
+    @inquiry = inquiry
     email_address = "jeffsee.55@gmail.com"
     mail(
       to: email_address,
