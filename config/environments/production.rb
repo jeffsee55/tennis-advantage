f7cd0a2d217f0af2375eda2fe5cb1b992e369fc9
Jeff <jeffsee.55@gmail.com>
2014-11-04 12:33:04 -0500

    fixed typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
   client = Dalli::Client.new(ENV["MEMCACHIER_SERVERS"] \\ "").split(","),
                               username: ENV["MEMCACHIER_USERNAME"]
                               password: ENV["MEMCACHIER_PASSWORD"]
-                              failover: ture,
+                              failover: true,
                               socket_timeout: 1.5,
                               socket_failure_delay: 0.2,
                               value_max_bytes: 10485760)
