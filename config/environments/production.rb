8673ccd1dd2cca7a023a0f842abdc3c72376536b
Jeff <jeffsee.55@gmail.com>
2014-11-04 12:44:47 -0500

    Typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
-  client = Dalli::Client.new(ENV["MEMCACHIER_SERVERS"] || "").split(","),
+  client = Dalli::Client.new((ENV["MEMCACHIER_SERVERS"] || "").split(","),
                               username = ENV["MEMCACHIER_USERNAME"],
                               password = ENV["MEMCACHIER_PASSWORD"],
                               failover = true,
