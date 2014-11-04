e5b9a0c8618b59ff744271f13e771e8feb0f2c1b
Jeff <jeffsee.55@gmail.com>
2014-11-04 12:34:42 -0500

    Fixed another typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,8 +19,8 @@ Rails.application.configure do
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   client = Dalli::Client.new(ENV["MEMCACHIER_SERVERS"] \\ "").split(","),
-                              username: ENV["MEMCACHIER_USERNAME"]
-                              password: ENV["MEMCACHIER_PASSWORD"]
+                              username: ENV["MEMCACHIER_USERNAME"],
+                              password: ENV["MEMCACHIER_PASSWORD"],
                               failover: true,
                               socket_timeout: 1.5,
                               socket_failure_delay: 0.2,
